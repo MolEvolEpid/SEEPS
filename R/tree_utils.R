0239dd0981cebbe48521fb86f809ec52803020e3
Michael Kupperman <mkupperman@lanl.gov>
2022-08-19 17:22:43 -0600

    Expand tests for phylo to newick, clean comments

--- R/tree_utils.R
+++ R/tree_utils.R
@@ -15,7 +15,7 @@ phylogeny_to_newick <- function(phylogeny, mode="mu") {
 
     M <- sum(phylogeny[, 6]) # number of leaf nodes
     inds <- which(phylogeny[, 6] != 0) # Get the index of the nonzeros
-    n_nodes = dim(phylogeny)[1] - 1
+    n_nodes <- dim(phylogeny)[1] - 1
 
     # We need to convert global indexing (1:# of tracked infections) to local indexing (1:M)
     global_to_local <- rep(0, max(inds))
@@ -25,23 +25,14 @@ phylogeny_to_newick <- function(phylogeny, mode="mu") {
     names <- lapply(1:M, function(x) paste0(x, "_"))
     branches[1:M] <- names
     merge_nodes <- vector("list", length = n_nodes)
-    # print(branches)
 
     for (index in 1:(n_nodes-1)) {
         parent <- phylogeny[index, 2]  # Get the parent
-        print("A")
-        print(c(index, parent))
-        if (is.null(merge_nodes[[parent]])) {
-            print("Y")
-            print(parent)
+        if (is.null(merge_nodes[[parent]])) { # First offspring
             merge_nodes[[parent]] <- index
         } else { # We alredy found one of the offspring
             # Record second offspring nodes
             children <- c(merge_nodes[[parent]], index)
-            print("X")
-            print(children)
-            print("Z")
-            print(parent)
             # Now construct the merged string
             branches[parent] <- paste0("(", branches[children[1]], ":", phylogeny[children[1], col], ",",
                                             branches[children[2]], ":", phylogeny[children[2], col], ")")
@@ -49,20 +40,6 @@ phylogeny_to_newick <- function(phylogeny, mode="mu") {
 
     }
 
-    # for (i in 1:n_nodes) {
-    #     left <- i
-    #     parent <- phylogeny[left, 2]  # Get the parent
-    #     if (branches[parent] == "") {
-    #         # Empty string
-    #         branches[parent] <- paste0("(", )
-    #     }
-
-
-    # # Similar approach from biophybreak
-    # for (i in 1:n_nodes) {
-    #     branch[i] <- paste0("(",left_name, ":",left_dist_to_root, ",",
-    #                         right_name,":", right_dist_to_root, ")")
-    # }
     # Complete the tree
     paste0(branches[n_nodes], ";")
 }
--- tests/testthat/test-phylogeny_to_newick.R
+++ tests/testthat/test-phylogeny_to_newick.R
@@ -21,12 +21,50 @@ three_leaf_phylo <- function() {
 
     newick_tree_mu <- "(3_:1,(1_:1,2_:1):1);"
     newick_tree_mean <- "(3_:1.1,(1_:1.1,2_:1.1):1.2);"
-    return(list("phylogeny"=phylogeny, "newick_tree_mu"=newick_tree_mu, "newick_tree_mean"=newick_tree_mean))
+    return(list("phylogeny" = phylogeny, "newick_tree_mu" = newick_tree_mu, "newick_tree_mean" = newick_tree_mean))
 }
 
+#' A helper function for tests. Builds a 4 leaf phylogeny with 2 internal nodes
+#' and the newick tree. All internal distances are unit. Uses the _ suffix for node names.
+four_leaf_phylo <- function() {
+    phylogeny <- matrix(0, nrow=8, ncol=6)
+    # Sample id's
+    phylogeny[1:7, 1] <- 1:7
+    # Contact network
+    phylogeny[1:2, 2] <- 5
+    phylogeny[3:4, 2] <- 6
+    phylogeny[5:6, 2] <- 7
+    # Sample times
+    phylogeny[1:4, 3] <- 3
+    phylogeny[4:5, 3] <- 2
+    phylogeny[6,   3] <- 1
+    # Set Expected # of mutations based on time/rates
+    phylogeny[1:4, 4] <- 1.1
 
-test_that("phylogeny_to_newick() minimal examples", {
+    phylogeny[5:6, 4] <- 1.2
+    # Actual amout of mutations
+    phylogeny[1:6, 5] <- 1
+    # leaf flags
+    phylogeny[1:4, 6] <- 1
+
+    newick_tree_mu <- "((1_:1,2_:1):1,(3_:1,4_:1):1);"
+    newick_tree_mean <- "((1_:1.1,2_:1.1):1.2,(3_:1.1,4_:1.1):1.2);"
+    return(list("phylogeny" = phylogeny, "newick_tree_mu" = newick_tree_mu, "newick_tree_mean" = newick_tree_mean))
+}
+
+test_that("phylogeny_to_newick() three leaf example", {
     reference <- three_leaf_phylo()
+    phylogeny <- reference[["phylogeny"]]
+    newick_tree_mu <- reference[["newick_tree_mu"]]
+    newick_tree_mean <- reference[["newick_tree_mean"]]
+    computed_tree <- phylogeny_to_newick(phylogeny, mode="mu")
+    expect_identical(newick_tree_mu, computed_tree)
+    computed_tree <- phylogeny_to_newick(phylogeny, mode="mean")
+    expect_identical(newick_tree_mean, computed_tree)
+})
+
+test_that("phylogeny_to_newick() four leaf example", {
+    reference <- four_leaf_phylo()
     phylogeny <- reference[["phylogeny"]]
     newick_tree_mu <- reference[["newick_tree_mu"]]
     newick_tree_mean <- reference[["newick_tree_mean"]]
