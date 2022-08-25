55d375230825b78e0e817ffb2a555c1200783953
Michael Kupperman <mkupperman@lanl.gov>
2022-08-25 16:14:44 -0600

    Include profiler test in refactor

--- tests/testthat/test-pipeline_profiler.R
+++ tests/testthat/test-pipeline_profiler.R
@@ -1,18 +1,3 @@
-# Here is a factory for the simulator result
-# mock 4 has large scale. You may want to skip this test.
-skip("For profiling only")
-get_mocked_simulator_result_4 <- function() {
-    set.seed(1947)  # for reproducability
-    biphasic_rate_function <- get_biphasic_HIV_rate(list("R0" = 4))
-
-    simulator_result <- gen_transmission_history_exponential_constant(
-        minimum_population = 50,
-        offspring_rate_fn = biphasic_rate_function,
-        maximum_population_target = 1000, total_steps = 120,
-        spike_root = FALSE)
-    return(simulator_result)
-}
-
 skip("For profiling only")
 test_that("Check bpb inputs for 50 tips", {
     simulator_result <- get_mocked_simulator_result_4()
