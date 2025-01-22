c92f4386671c3b87d3b0a04f1d77994595e852ae
Michael Kupperman <mkupperman@lanl.gov>
2025-01-22 15:14:46 -0700

    Update vignette metadata, readme text

--- data-raw/prepare_data.R
+++ data-raw/prepare_data.R
@@ -1,7 +1,9 @@
 load_local_data <- function() {
     # Read all files with prefix seq_ in from current directory
-    files <- list.files("data-raw", pattern = "seq_.*\\.(csv|fasta|metadata)",
-                        recursive = TRUE)
+    files <- list.files("data-raw",
+        pattern = "seq_.*\\.(csv|fasta|metadata)",
+        recursive = TRUE
+    )
     # Get the unique organism names
     organism_names <- unique(strsplit(tools::file_path_sans_ext(files), "seq_"))
     # Loop over organism names and load in the data
@@ -11,23 +13,28 @@ load_local_data <- function() {
         organism_name <- organism_name[2]
         # Get the organism metadata
         metadata <- paste(readLines(paste0("data-raw/seq_", organism_name, ".metadata")),
-                          collapse = "\n")
+            collapse = "\n"
+        )
         # Get the organism fasta file
         fasta <- ape::read.dna(paste0("data-raw/seq_", organism_name, ".fasta"),
-                                 as.character = TRUE, format = "fasta")
+            as.character = TRUE, format = "fasta"
+        )
         sequence <- as.character((unlist(as.list(fasta))))
 
         # Get the organism csv file
         csv <- utils::read.csv(paste0("data-raw/seq_", organism_name, ".csv"),
-                          header = TRUE, sep = ",")
+            header = TRUE, sep = ","
+        )
         # Get the organism accession ID
         accession_id <- rownames(fasta)
         # Get the organism sequence
         sequence <- sequence
         # Get the organism regions
         # Add the organism to the package
-        seq_store <- add_organism(seq_store, organism_name, accession_id,
-                                  sequence, csv, metadata)
+        seq_store <- add_organism(
+            seq_store, organism_name, accession_id,
+            sequence, csv, metadata
+        )
     }
     # Save to internal data
     usethis::use_data(seq_store, overwrite = TRUE, internal = TRUE)
@@ -37,9 +44,9 @@ load_local_data <- function() {
 add_organism <- function(seq_store, organism_name, accession_id,
                          sequence, region_map, metadata) {
     # Add the organism to the package's internal environment
-    seq_store[[organism_name]] <- list(  # nolint: object_usage_linter
+    seq_store[[organism_name]] <- list( # nolint: object_usage_linter
         accession_id = accession_id,
-        sequence = toupper(sequence),  # ape likes to load in lowercase
+        sequence = toupper(sequence), # ape likes to load in lowercase
         region = region_map,
         metadata = metadata
     )
